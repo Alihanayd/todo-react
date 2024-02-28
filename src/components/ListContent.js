9fa3f3d15968026d20e934495e769e7b6d4a16b1
alihanayd <aaydin184@posta.pau.edu.tr>
2024-02-29 01:08:59 +0300

    update():: filtering buttons

--- src/components/ListContent.js
+++ src/components/ListContent.js
@@ -1,34 +1,64 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import ListItem from "./ListItem";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { setFilter, clearCompleted } from "../store/actions/todoActions";
 
 const ListContent = () => {
   const { todos, todoFilter } = useSelector((state) => state.todo);
-  const [list, setList] = useState([]);
-  useEffect(() => {
-    if (todoFilter === "all") {
-      setList(todos);
-      return;
-    }
-    setList(
-      todos.filter((todo) => todo.completed === (todoFilter === "completed"))
-    );
-  }, [todos, todoFilter]);
+  const dispatch = useDispatch();
 
   return (
-    <div>
-      {list.length > 0 && (
-        <div className="bg-gray-700 p-4 rounded">
-          <h2 className="text-2xl font-bold mb-4 text-white">To-Do List</h2>
+    <div className="w-3/4">
+      {todos.length > 0 && (
+        <div className="bg-gray-700 p-4 rounded w-full max-h-[500px]">
+          <div className="flex justify-center space-x-2 mb-4">
+            <button
+              className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+              onClick={() => {
+                dispatch(setFilter("all"));
+              }}
+            >
+              Filter All
+            </button>
+            <button
+              className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+              onClick={() => {
+                dispatch(setFilter("completed"));
+              }}
+            >
+              Filter Completed
+            </button>
+            <button
+              className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
+              onClick={() => {
+                dispatch(setFilter("incompleted"));
+              }}
+            >
+              Filter Incomplete
+            </button>
+            <button
+              className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
+              onClick={() => {
+                dispatch(clearCompleted());
+              }}
+            >
+              Clear Completed
+            </button>
+          </div>
           <ul className="space-y-2">
-            <li className="bg-gray-800 p-2 rounded shadow">
-              {list.map((todo) => (
-                <ListItem
-                  key={todo.id}
-                  todo={todo}
-                  className={todo.completed ? "completed" : ""}
-                />
-              ))}
+            <li className="bg-gray-800 p-2 rounded shadow h-[416px] overflow-auto">
+              {todos.map((todo) => {
+                if (todoFilter === "all") {
+                  return <ListItem key={todo.id} todo={todo} />;
+                }
+                if (todoFilter === "completed" && todo.completed) {
+                  return <ListItem key={todo.id} todo={todo} />;
+                }
+                if (todoFilter === "incompleted" && !todo.completed) {
+                  return <ListItem key={todo.id} todo={todo} />;
+                }
+                return null;
+              })}
             </li>
           </ul>
         </div>
