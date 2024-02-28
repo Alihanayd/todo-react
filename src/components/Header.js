27730a3d535f93aa9209e8105b841be582032ef0
alihanayd <aaydin184@posta.pau.edu.tr>
2024-02-28 22:57:38 +0300

    fix(header):: text

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const Header = () => {
-    return (
-        <div className=''>
-            <h1 className='text-white text-2xl font-bold tracking-widest'>To-Do App</h1>
-        </div>
-    );
+  return (
+    <div className="">
+      <h1 className="text-white text-2xl font-bold tracking-widest">
+        TODO LIST
+      </h1>
+    </div>
+  );
 };
 
 export default Header;
