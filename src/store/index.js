ed5d65dbcb2667c69133f0637853aac9b5969902
alihanayd <aaydin184@posta.pau.edu.tr>
2024-02-29 16:46:22 +0300

    add():: local storage

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,18 @@
 import todoReducer from "./reducers/todoReducer";
 import { combineReducers, createStore } from "redux";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
 
 const reducer = combineReducers({
-    todo: todoReducer,
+  todo: todoReducer,
 });
 
-export const store = createStore(reducer);
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["todo"],
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+export const store = createStore(persistedReducer);
+export const persistor = persistStore(store);
